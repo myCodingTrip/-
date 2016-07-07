809b6b02383474d5fc856a25d2517b6a06aee45b
My <441015782@qq.com>
2016-07-07 19:55:51 +0800

    77

--- src/cn/eoe/connect/bluetooth/device/MainActivity.java
+++ src/cn/eoe/connect/bluetooth/device/MainActivity.java
@@ -32,62 +32,58 @@ public class MainActivity extends Activity implements OnItemClickListener {
 
 	private ListView lvDevices;
 	private EditText etMessage;
-	private BluetoothAdapter bluetoothAdapter;
+	
 	//存储蓝牙设备的地址
 	private List<String> bluetoothDevices = new ArrayList<String>();
 	private ArrayAdapter<String> arrayAdapter;
+	private final String NAME = "Bluetooth_Socket";
 	//指定一个UUID
 	private final UUID MY_UUID = UUID
 			.fromString("db764ac8-4b08-7f25-aafe-59d03c27bae3");
-//	private final UUID MY_UUID = UUID.randomUUID();
-	private final String NAME = "Bluetooth_Socket";
-	private BluetoothSocket clientSocket;
-	private BluetoothDevice device;  
-    private AcceptThread acceptThread; 
-	private OutputStream os;
- 
+	private AcceptThread acceptThread; 
+	
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
 		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 		setContentView(R.layout.main);
-
 		bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-		etMessage = (EditText) findViewById(R.id.edit_text);
-		lvDevices = (ListView) findViewById(R.id.lvDevices);
-		//显示已配对的蓝牙设备
-		Set<BluetoothDevice> pairedDevices = bluetoothAdapter
-				.getBondedDevices();
-
-		if (pairedDevices.size() > 0) {
-			for (BluetoothDevice device : pairedDevices) {
-				bluetoothDevices.add(device.getName() + ":"
-						+ device.getAddress() + "\n");
-
-			}
-		}
+		getBondedDevices();
 		//将所有设备显示在列表上
 		arrayAdapter = new ArrayAdapter<String>(this,
 				android.R.layout.simple_list_item_1, android.R.id.text1,
 				bluetoothDevices);
-
-		lvDevices.setAdapter(arrayAdapter);
-		lvDevices.setOnItemClickListener(this);
+		bindView();
+		//注册两个广播接收器
         IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
         this.registerReceiver(receiver, filter);
-
         filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
         this.registerReceiver(receiver, filter);
         //启动服务
 		acceptThread = new AcceptThread();
 		acceptThread.start();
 	}
-
+	//获取已配对的蓝牙设备
+	private void getBondedDevices() {
+		Set<BluetoothDevice> pairedDevices = bluetoothAdapter
+				.getBondedDevices();
+		if (pairedDevices.size() > 0) {
+			for (BluetoothDevice device : pairedDevices) {
+				bluetoothDevices.add(device.getName() + ":"
+						+ device.getAddress() + "\n");
+			}
+		}
+	}
+	private void bindView() {
+		etMessage = (EditText) findViewById(R.id.edit_text);
+		lvDevices = (ListView) findViewById(R.id.lvDevices);
+		lvDevices.setAdapter(arrayAdapter);
+		lvDevices.setOnItemClickListener(this);
+	}
 	public void onClick_Search(View view) {
 		setProgressBarIndeterminateVisibility(true);
 		setTitle("正在扫描...");
-
 		if (bluetoothAdapter.isDiscovering()) {
 			bluetoothAdapter.cancelDiscovery();
 		}
@@ -114,6 +110,10 @@ public class MainActivity extends Activity implements OnItemClickListener {
 			}
 		}
 	};
+	private BluetoothAdapter bluetoothAdapter;
+	private BluetoothSocket clientSocket;
+	private BluetoothDevice device;  
+	private OutputStream os;
 	//列表单击事件（客户端）
 	@Override
 	public void onItemClick(AdapterView<?> parent, View view, int position,
@@ -144,16 +144,16 @@ public class MainActivity extends Activity implements OnItemClickListener {
 				String DATA = etMessage.getText().toString();
 				//生成3DES密钥
 				byte[] tridesKey = TripleDESUtil.initKey();
-				//System.out.println("发送tridesKey:"+printByte(tridesKey));
+				System.out.println("发送tridesKey:"+printByte(tridesKey));
 				//生成3DES密文
 				byte[] tridesResult = TripleDESUtil.encrypt(DATA.getBytes(), tridesKey);
-				//System.out.println("发送tridesResult:"+printByte(tridesResult));
+				System.out.println("发送tridesResult:"+printByte(tridesResult));
 				//整合发送
 				byte[] b = new byte[24+tridesResult.length];//24+8
 				int i;
 				for(i=0; i<tridesKey.length; i++)	b[i] = tridesKey[i];
 				for(int j=0; j<tridesResult.length; j++)	b[i+j] = tridesResult[j];
-				//System.out.println("bs:"+printByte(b));
+				System.out.println("bs:"+printByte(b));
 				os.write(b);
 				Toast.makeText(this, "信息已发送",
 						Toast.LENGTH_SHORT).show();
@@ -161,7 +161,7 @@ public class MainActivity extends Activity implements OnItemClickListener {
 		} catch (Exception e) {
 		}
 	}
-	
+
 	private Handler handler = new Handler() {
 		public void handleMessage(Message msg) {
 			Toast.makeText(MainActivity.this, String.valueOf(msg.obj),
@@ -198,10 +198,10 @@ public class MainActivity extends Activity implements OnItemClickListener {
 					int count = is.read(b);
 					//获取密钥
 					byte[] tridesKey = new byte[24];
-					//System.out.println("获取tridesKey:"+printByte(tridesKey));
+					System.out.println("获取tridesKey:"+printByte(tridesKey));
 					//获取密文
 					byte[] tridesResult = new byte[count-24];
-					//System.out.println("获取tridesResult:"+printByte(tridesResult));
+					System.out.println("获取tridesResult:"+printByte(tridesResult));
 					for(int i=0; i<24; i++)	  tridesKey[i] = b[i];
 					for(int j=24; j<count; j++)	  tridesResult[j-24] = b[j];
 					byte[] tridesPlain = TripleDESUtil.decrypt(tridesResult, tridesKey);
@@ -214,5 +214,8 @@ public class MainActivity extends Activity implements OnItemClickListener {
 			}
 		}
 	}
-
+	private String printByte(byte[] b) {
+		String string = BytesToHex.fromBytesToHex(b);
+		return string+" "+string.length();
+	}
 }
